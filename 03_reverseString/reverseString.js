75f70ab5571b96fe84f13fd65313a485d6845806
seyfire <97469939+Seyfire@users.noreply.github.com>
2024-11-04 15:20:38 -0500

    complete excercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  let newString = "";
+  let arrString = [];
 
+  // convert string to reversed array
+  for (let i = 0; i < str.length; i++) {
+      arrString.unshift(str.at(i));
+    }
+  
+  // convert array back into string
+  for (let i = 0; i < arrString.length; i++) {
+      newString += arrString[i]; 
+    }
+  
+  return newString;
 };
 
 // Do not edit below this line

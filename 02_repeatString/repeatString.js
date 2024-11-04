f23499e854c440a615f69a286765a77580ea22b9
seyfire <97469939+Seyfire@users.noreply.github.com>
2024-11-03 23:39:45 -0500

    starter code

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+	// create empty string var
+	// for loop appends string num amount of times to the var
+	// return the variable
+	for ( let i = 0; i < num; i++) {}
 };
 
 // Do not edit below this line

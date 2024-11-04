409de281b1de7da03daca05e4c6534b735d56791
seyfire <97469939+Seyfire@users.noreply.github.com>
2024-11-04 17:44:46 -0500

    complete excercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let filteredArray = array;
+
+    for (let i = 1; i < arguments.length; i++)  {
+        let filterNum = arguments[i];
+        filteredArray = filteredArray.filter(function (num) {
+            return num !== filterNum; 
+        });
+    }
+
+    return filteredArray;
 };
 
 // Do not edit below this line

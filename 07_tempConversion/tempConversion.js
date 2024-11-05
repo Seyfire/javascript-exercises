e113459944f441e24be9df282201d99b47914c72
seyfire <97469939+Seyfire@users.noreply.github.com>
2024-11-05 18:14:12 -0500

    complete excercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
 const convertToCelsius = function(temp) {
-  return ( (temp - 32) * 5/9 ).toFixed(1);
+  let convertedTemp = ( (temp - 32) * 5/9 );
+  if ( Number.isInteger(convertedTemp) ) {
+    return convertedTemp;
+  } else {
+    return Math.round( convertedTemp * 10 ) / 10;
+  }
 };
 
 const convertToFahrenheit = function(temp) {
-  return (temp * 9/5 + 32).toFixed(1);
+  let convertedTemp = (temp * 9/5 + 32);
+  if ( Number.isInteger(convertedTemp) ) {
+    return convertedTemp;
+  } else {
+    return Math.round( convertedTemp * 10 ) / 10;
+  }
 };
 
 // Do not edit below this line
